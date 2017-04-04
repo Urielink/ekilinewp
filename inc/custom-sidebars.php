11317e09ce3de3305b6630dfae449c194415a953
Urielink <urielink81@gmail.com>
2017-04-04 18:00:29 -0500

    acomodando los scripts, de sidebars sobre todo.

--- inc/custom-sidebars.php
+++ inc/custom-sidebars.php
@@ -5,15 +5,46 @@
  * @package ekiline
  */
 
- /** Sidebars dinamicas **/
+// cada sidebar tiene una variable.
+
+$leftOn = get_theme_mod('ekiline_sidebarLeft','on');
+$rightOn = get_theme_mod('ekiline_sidebarRight','on');
+
+
+/* Función especial, condicionar el uso de clases CSS de acuerdo a formatos o contenidos específicos
+ * Se inyecta una CSS en el body
+ * https://codex.wordpress.org/Plugin_API/Filter_Reference/body_class
+ * https://developer.wordpress.org/reference/functions/body_class/
+ */
+
+add_filter( 'body_class', function( $classes ) {
+    
+    //Llamo a mis variables
+    global $leftOn, $rightOn;
+    
+    
+    if ( $leftOn == 'off' && $rightOn == 'on' ) {
+        return array_merge( $classes, array( 'toggle-sidebars left-on' ) );
+    } elseif ( $rightOn == 'off' && $leftOn == 'on' ) {
+        return array_merge( $classes, array( 'toggle-sidebars right-on' ) );
+    } elseif ( $rightOn == 'off' && $leftOn == 'off' ) {
+        return array_merge( $classes, array( 'toggle-sidebars right-on left-on' ) );
+    } else {
+        return array_merge( $classes, array( 'show-sidebars' ) );
+    }   
+    
+} );
+
+
 
 /* En caso de que el sidebar esté activo, añade la clase columna de 9: 
  *index.php, single.php, search.php, page.php, archive.php, 404.php */
 
 function sideOn() {
     // debo sobreescribir la clase que actúa en conjunto con el grid, para darle prioridad al contenido (SEO).
-    $leftOn = get_theme_mod('ekiline_sidebarLeft','on');
-        if ($leftOn != 'off') { $gridFix = 'col-sm-push-3 '; }
+    global $leftOn;
+    
+    if ($leftOn != 'off') { $gridFix = 'col-sm-push-3 '; }
     
     if ( is_active_sidebar( 'sidebar-1' ) && !is_active_sidebar( 'sidebar-2' ) ) {
              $sideon = ' col-sm-9 side1'; 
@@ -28,8 +59,9 @@ function sideOn() {
 function gridCss() {
 
     // debo sobreescribir la clase que actúa en conjunto con el grid, para darle prioridad al contenido (SEO).
-    $rightOn = get_theme_mod('ekiline_sidebarRight','on');
-        if ($rightOn != 'off') { $gridFix = ' col-sm-pull-6'; } else { $gridFix = ' col-sm-pull-9';}
+    global $rightOn;
+    
+    if ($rightOn != 'off') { $gridFix = ' col-sm-pull-6'; } else { $gridFix = ' col-sm-pull-9';}
 
     if ( is_active_sidebar( 'sidebar-1' ) && !is_active_sidebar( 'sidebar-2' ) ) {
              $gridCss = ' col-sm-3 col-sm-pull-6'; 
@@ -45,9 +77,7 @@ function gridCss() {
 
 function add_sidebar_action( $items, $args ) {
 
-    $leftOn = get_theme_mod('ekiline_sidebarLeft','on');
-    $rightOn = get_theme_mod('ekiline_sidebarRight','on');
-
+    global $leftOn, $rightOn;
     
     if ($leftOn == 'off') {
         $items .= '<li><a href="#" id="show-sidebar-left">'.esc_html__( 'Sidebar Left', 'ekiline' ).'</a></li>';
@@ -65,9 +95,7 @@ add_filter( 'wp_nav_menu_items', 'add_sidebar_action', 10, 2 );
 
 function sidebarButtons(){
 
-    $leftOn = get_theme_mod('ekiline_sidebarLeft','on');
-    $rightOn = get_theme_mod('ekiline_sidebarRight','on');
-
+    global $leftOn, $rightOn;
     
     if ($leftOn == 'off') {
         $items .= '<a href="#" id="show-sidebar-left" class="btn btn-default btn-sbleft">'.esc_html__( 'Sidebar Left', 'ekiline' ).'</a>';
@@ -80,27 +108,3 @@ function sidebarButtons(){
     echo $items; 
 }
 
-
-
-/* Función especial, condicionar el uso de clases CSS de acuerdo a formatos o contenidos específicos
- * Se inyecta una CSS en el body
- * https://codex.wordpress.org/Plugin_API/Filter_Reference/body_class
- * https://developer.wordpress.org/reference/functions/body_class/
- */
-
-add_filter( 'body_class', function( $classes ) {
-    
-    $leftOn = get_theme_mod('ekiline_sidebarLeft','on');
-    $rightOn = get_theme_mod('ekiline_sidebarRight','on');
-    
-    if ( $leftOn == 'off' && $rightOn == 'on' ) {
-        return array_merge( $classes, array( 'toggle-sidebars left-on' ) );
-    } elseif ( $rightOn == 'off' && $leftOn == 'on' ) {
-        return array_merge( $classes, array( 'toggle-sidebars right-on' ) );
-    } elseif ( $rightOn == 'off' && $leftOn == 'off' ) {
-        return array_merge( $classes, array( 'toggle-sidebars right-on left-on' ) );
-    } else {
-        return array_merge( $classes, array( 'show-sidebars' ) );
-    }   
-    
-} );
--- inc/customizer.php
+++ inc/customizer.php
@@ -24,3 +24,190 @@ function ekiline_customize_preview_js() {
 	wp_enqueue_script( 'ekiline_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
 add_action( 'customize_preview_init', 'ekiline_customize_preview_js' );
+
+
+/** Personalizar el tema desde las opciones, logo, fondo, etc **/
+
+function ekiline_theme_customizer( $wp_customize ) {
+    
+// colores  
+    $colors = array();
+    $colors[] = array( 'slug'=>'text_color', 'default' => '#7c8d96', 'label' => __( 'Color de texto', 'ekiline' ) );
+    $colors[] = array( 'slug'=>'links_color', 'default' => '#f8af0c', 'label' => __( 'Color de links', 'ekiline' ) );
+    $colors[] = array( 'slug'=>'module_color', 'default' => '#d0d7dd', 'label' => __( 'Color de modulos', 'ekiline' ) );
+    
+    foreach($colors as $color)
+    {
+        // SETTINGS
+        $wp_customize->add_setting( $color['slug'], array( 'default' => $color['default'], 'type' => 'option', 'capability' => 'edit_theme_options' ));
+
+        // CONTROLS
+        $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, $color['slug'], array( 'label' => $color['label'], 'section' => 'colors', 'settings' => $color['slug'] )));
+    }
+
+// añadir un controlador: https://codex.wordpress.org/Class_Reference/WP_Customize_Control  
+// https://make.wordpress.org/core/2014/07/08/customizer-improvements-in-4-0/
+// https://developer.wordpress.org/themes/advanced-topics/customizer-api/
+
+    $wp_customize->add_setting( 'ekiline_logo_max' );
+
+    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'ekiline_logo_max', array(
+            'label'    => __( 'Imagen de logo horizontal', 'ekiline' ),
+            'description' => 'Esta imagen se recomienda para su vista en ordenadores de escritorio.',
+            'section'  => 'title_tagline', // esta seccion corresponde a una predeterminada.
+            'settings' => 'ekiline_logo_max',
+            'priority' => 100,
+    ) ) );
+
+    $wp_customize->add_setting( 'ekiline_logo_min' );
+
+    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'ekiline_logo_min', array(
+        'label'    => __( 'Imagen de logo vertical', 'ekiline' ),
+        'description' => 'Esta imagen se recomienda para su vista en dispositivos móviles.',
+        'section'  => 'title_tagline', // esta seccion corresponde a una predeterminada.
+        'settings' => 'ekiline_logo_min',
+        'priority' => 100,
+    ) ) );  
+        
+ // video
+    $wp_customize->add_section( 'ekiline_video_portada' , array(
+            'title'       => __( 'Video de cabecera', 'ekiline' ),
+            'priority'    => 90,
+            'description' => '<b>Debes tener una imagen de cabecera</b>.<br/>Elige un archivo de video <b>MP4, WEBM u OGV</b> de tu biblioteca. La imagen de cabecera se adaptará como fondo en caso de que los dispositivos no puedan reproducir video.',
+    ) );
+    
+    $wp_customize->add_setting( 'ekiline_video' );
+    
+    $wp_customize->add_control( new WP_Customize_Upload_Control( $wp_customize, 'ekiline_video', array(
+            'label'    => __( 'Video MP4, WEBM u OGV', 'ekiline' ),
+            'section'  => 'ekiline_video_portada',
+            'settings' => 'ekiline_video',
+    ) ) );  
+
+
+// ancho de la página
+    $wp_customize->add_section( 'ekiline_vista_section' , array(
+            'title'       => __( 'Vista de tu sitio', 'ekiline' ),
+            'priority'    => 50,
+            'description' => 'Elige si quieres que el homepage, los listados o el articulo principal se desplieguen a todo lo ancho (fullwidth) o ajustada al centro.',
+    ) );
+    
+        $wp_customize->add_setting(
+            'ekiline_anchoHome', array(
+                    'default' => 'container',
+                ) );
+        
+        $wp_customize->add_control(
+            'ekiline_anchoHome',
+            array(
+                'type' => 'radio',
+                'label' => 'Homepage',
+                'section' => 'ekiline_vista_section',
+                'choices' => array(
+                    'container' => 'Ajustado',
+                    'container-fluid' => 'A todo lo ancho',
+                ),
+            )
+        );      
+        
+        $wp_customize->add_setting(
+            'ekiline_anchoCategory', array(
+                    'default' => 'container',
+                ) );
+        
+        $wp_customize->add_control(
+            'ekiline_anchoCategory',
+            array(
+                'type' => 'radio',
+                'label' => 'Categorías o listados',
+                'section' => 'ekiline_vista_section',
+                'choices' => array(
+                    'container' => 'Ajustado',
+                    'container-fluid' => 'A todo lo ancho',
+                ),
+            )
+        ); 
+        
+        $wp_customize->add_setting(
+            'ekiline_anchoSingle', array(
+                    'default' => 'container',
+                ) );
+        
+        $wp_customize->add_control(
+            'ekiline_anchoSingle',
+            array(
+                'type' => 'radio',
+                'label' => 'Páginas individuales',
+                'section' => 'ekiline_vista_section',
+                'choices' => array(
+                    'container' => 'Ajustado',
+                    'container-fluid' => 'A todo lo ancho',
+                ),
+            )
+        );         
+        
+        // ocultar y mostrar sidebars
+
+        $wp_customize->add_setting(
+            'ekiline_sidebarLeft', array(
+                    'default' => 'on',
+                ) );
+        
+        $wp_customize->add_control(
+            'ekiline_sidebarLeft',
+            array(
+                'type' => 'radio',
+                'label' => 'Sidebar izquierdo',
+                'section' => 'ekiline_vista_section',
+                'choices' => array(
+                    'on' => 'Visible',
+                    'off' => 'Plegable',
+                ),
+            )
+        );     
+        
+        $wp_customize->add_setting(
+            'ekiline_sidebarRight', array(
+                    'default' => 'on',
+                ) );
+        
+        $wp_customize->add_control(
+            'ekiline_sidebarRight',
+            array(
+                'type' => 'radio',
+                'label' => 'Sidebar derecho',
+                'section' => 'ekiline_vista_section',
+                'choices' => array(
+                    'on' => 'Visible',
+                    'off' => 'Plegable',
+                ),
+            )
+        );                      
+
+    // Optimización, códigos de seguimiento
+   
+    $wp_customize->add_section( 'ekiline_tracking_section' , array(
+            'title'       => __( 'Optimización', 'ekiline' ),
+            'priority'    => 150,
+            'description' => __( 'Añade los códigos de seguimiento que te ayudarán a optimizar y dar seguimiento a este sitio web.', 'ekiline' )
+    ) );
+
+    // Código de analytics  
+    $wp_customize->add_setting( 
+        'ekiline_analytics', array(
+            'default' => ''
+        ) );
+    
+    $wp_customize->add_control(
+        'ekiline_analytics',
+            array(
+                'label'          => __( 'Trackeo de Google', 'ekiline' ),
+                'description'    => 'Inserta el código de Google analytics, solo tu identificador (UA-XXXXX-XX)',
+                'section'        => 'ekiline_tracking_section',
+                'settings'       => 'ekiline_analytics',
+                'type'           => 'text'
+            )
+        );      
+    
+}
+add_action('customize_register', 'ekiline_theme_customizer');
