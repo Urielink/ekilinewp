122477c261fd622f514c93e32540fbbd94175970
Urielink <urielink81@gmail.com>
2017-06-03 20:02:18 -0500

    separé los archvis

--- js/theme.js
+++ js/theme.js
@@ -281,44 +281,7 @@
 		$( '.nav-links' ).addClass( 'pager');		
 		$( '.nav-links .nav-next' ).addClass( 'next');		
 		$( '.nav-links .nav-previous' ).addClass( 'previous');		
-			   
-		
-		/** 
-		 * experimento array de rutas
-		 * http://api.jquery.com/JQuery.each/
-		 */
-
-//		var obj = {
-//				  "handler1": "rutahandler1",
-//				  "handler2": "rutahandler2"
-//				};		
-
-//		$.each( obj, function( key, value ) {
-//				// alert( key + ": " + value );
-//
-//			var $head = $("head");
-//			var $wpcss = $head.find("style[id='ekiline-inline']"); 
-//			var $cssinline = $head.find("style:last");
-//			var $ultimocss = $head.find("link[rel='stylesheet']:last");
-//			var linkCss = "<link id='"+ key +"' rel='stylesheet' href='"+ value +"' type='text/css' media='screen'>";
-//
-//	        // En caso de de encontrar una etiqueta de estilo ó link ó nada inserta el otro estilo css, 
-//
-//			if ($wpcss.length){ 
-//					$wpcss.before(linkCss); 
-//				} else if ($cssinline.length){ 
-//					$cssinline.before(linkCss); 
-//				} else if ($ultimocss.length){ 
-//					$ultimocss.before(linkCss); 
-//				} else { 
-//					$head.append(linkCss); 
-//				}		
-//		
-//			
-//			
-//		});
-		
-			
+			        
 	    
 
 	}); 			
