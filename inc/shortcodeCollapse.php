d204297e10ae1dff9d663231e9e62c5d6b2f96bf
Urielink <urielink81@gmail.com>
2017-06-06 15:50:01 -0500

    creados el shortcode de acordeones

--- inc/shortcodeCollapse.php
+++ inc/shortcodeCollapse.php
@@ -18,32 +18,51 @@
 */
 
 function gcollpase_shortcode( $atts, $content = null ) {
-    // 1. reemplazamos el random matematico para que la ejecucion sea con el nombre del tab.    
+
     // $unique = mt_rand();
-    extract( shortcode_atts( array( 'class' => '' ), $atts ) );
+    extract( shortcode_atts( array( 'class' => '', 'id' => 'accordion' ), $atts ) );
+    $groupId = $id;
     $regex = '\\[(\\[?)(collpasecontent)\\b([^\\]\\/]*(?:\\/(?!\\])[^\\]\\/]*)*?)(?:(\\/)\\]|\\](?:([^\\[]*+(?:\\[(?!\\/\\2\\])[^\\[]*+)*+)\\[\\/\\2\\])?)(\\]?)';
     preg_match_all("/$regex/is",$content,$match);
     $content = $match[0];
         
-   $return =  '<div class="tabs-module clearfix '.$class.'">';
+   $return =  '<div class="panel-group '.$class.'" id="'.$groupId.'" role="tablist" aria-multiselectable="true">';
+   
         $i = -1;
-        $return .= '<ul class="nav nav-tabs">';
-                        foreach($content as $c){ $i++;
-                            //El nombre del tab va despues de $attr
-                            //$unique_id = 'tab_tab_'.$unique.'_'.$i;
-                            preg_match('/\[collpasecontent ([^\\]\\/]*(?:\\/(?!\\])[^\\]\\/]*)*?)/',$c,$matchattr);
-                            $attr = shortcode_parse_atts($matchattr[1]);
-                            //Filtramos los caracteres y añadimos un contador.
-                            $unique_id = ekiline_cleanspchar($attr['title']).'-'.$i;
-                            $return .= '<li '.(($i==0) ? 'class="active"' : '').'><a href="#'.$unique_id.'" data-toggle="tab">'.$attr['title'].'</a></li>';
-                            $content[$i] = str_replace('[collpasecontent ','[collpasecontent '.(($i==0) ? 'class="active"' : '').' id="'.$unique_id.'" ',$content[$i]);
-                        }
-        $return .= '</ul>';
-        $return .= '<div class="tab-content">';
-                        foreach($content as $c){
-                            $return .= do_shortcode($c);
-                        }
-        $return .= '</div></div>';
+
+            foreach($content as $c){ $i++;
+            
+                //El nombre del tab va despues de $attr
+                //$unique_id = 'tab_tab_'.$unique.'_'.$i;
+                preg_match('/\[collpasecontent ([^\\]\\/]*(?:\\/(?!\\])[^\\]\\/]*)*?)/',$c,$matchattr);
+                $attr = shortcode_parse_atts($matchattr[1]);
+                //Filtramos los caracteres y añadimos un contador.
+                $unique_id = ekiline_cleanspchar($attr['title']).'-'.$i;
+                //creo un id grupal para cada item
+                $heading_id = ekiline_cleanspchar($attr['title']).$i.$i;
+                                
+                $return .= '<div class="panel panel-default">';
+                
+                    $return .= '<div class="panel-heading" role="tab" id="'.$heading_id.'">
+                                    <h4 class="panel-title">';
+                    
+                        $return .= '<a '.(($i!=0) ? 'class="collapsed"' : '').' role="button" data-toggle="collapse" data-parent="#'.$groupId.'" href="#'.$unique_id.'" aria-expanded="true" aria-controls="'.$unique_id.'">'.$attr['title'].'</a>';
+                        
+                        $return .= '</h4>
+                                </div>'; //panel-heading
+
+                    
+                    $return .= '<div id="'.$unique_id.'" class="panel-collapse collapse '.(($i==0) ? 'in' : '').' '.$class.'" role="tabpanel" aria-labelledby="'.$heading_id.'">';
+                                    $return .= do_shortcode($c);
+                    $return .= '</div>';
+
+                                        
+                $return .= '</div>'; //panel-default
+                                                        
+            }
+                        
+
+        $return .= '</div>'; //panel-group
         
    return $return;
 }
@@ -52,10 +71,10 @@ add_shortcode('modulegroupcollapse',  'gcollpase_shortcode');
 
 
 function collpasecontent_shortcode( $atts, $content = null ) {
-    extract( shortcode_atts( array( 'title' => '', 'id' =>'', 'class' =>'' ), $atts ) );
-        $return = '<div class="tab-pane '.$class.'" id="'.$id.'">';
-        $return .= '<div class="panel-body">'.do_shortcode($content).'</div>';
-        $return .= '</div>';
+    extract( shortcode_atts( array( 'title' => ''), $atts ) );
+
+        $return = '<div class="panel-body">'.do_shortcode($content).'</div>';
+
     return $return;
 }
 
