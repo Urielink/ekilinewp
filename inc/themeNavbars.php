2b0d81f2ef124b420aac42bcba896d514f25319e
Urielink <urielink81@gmail.com>
2017-08-16 22:00:30 -0500

    ajustes en estilo del navbar

--- inc/themeNavbars.php
+++ inc/themeNavbars.php
@@ -114,34 +114,35 @@ function primaryNavbar(){
     if ( has_nav_menu( 'primary' ) ) : ?>
     
             <nav id="site-navigation-primary"  class="navbar <?php echo $inverseMenu;?> primary-navbar" role="navigation">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse.primary">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php logoTheme(); ?></a>
-                </div><!-- .navbar-header -->
-                
-    
-                <!-- The WordPress Menu goes here -->
-                <?php wp_nav_menu( array(
-                        'menu'              => 'primary',
-                        'theme_location'    => 'primary',
-                        'depth'             => 2,
-                        'container'         => 'div',
-                            'container_class'   => 'collapse navbar-collapse primary',
-                            'container_id'      => 'navbar-collapse-in',
-                        'menu_class'        => 'nav navbar-nav',
-                        'menu_id'           => 'main-menu',
-                        'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                        'walker'            => new WP_Bootstrap_Navwalker()
-                        )
-                      ); ?>                           
-                
-        		<?php dynamic_sidebar( 'navwidget-nw2' ); ?>     
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse.primary">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php logoTheme(); ?></a>
+                    </div><!-- .navbar-header -->
+                    
         
+                    <!-- The WordPress Menu goes here -->
+                    <?php wp_nav_menu( array(
+                            'menu'              => 'primary',
+                            'theme_location'    => 'primary',
+                            'depth'             => 2,
+                            'container'         => 'div',
+                                'container_class'   => 'collapse navbar-collapse primary',
+                                'container_id'      => 'navbar-collapse-in',
+                            'menu_class'        => 'nav navbar-nav navbar-right',
+                            'menu_id'           => 'main-menu',
+                            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                            'walker'            => new WP_Bootstrap_Navwalker()
+                            )
+                          ); ?>                           
+                    
+            		<?php dynamic_sidebar( 'navwidget-nw2' ); ?>     
+                </div>
             </nav><!-- .site-navigation -->		
             
     <?php endif;
