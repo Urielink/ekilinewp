196850940c69caa4c5668d7ff73aaf1ce32f730c
Urielink <urielink81@gmail.com>
2017-08-23 18:55:26 -0500

    restaurar

--- inc/serviceOptimize.php
+++ inc/serviceOptimize.php
@@ -152,12 +152,12 @@ function wsds_defer_scripts( $tag, $handle, $src ) {
         'theme-scripts',
 		'google-analytics',
 		'optimizar',
-		'contact-form-7',
-		'wc-add-to-cart',
-		'woocommerce',
+        'contact-form-7',
+        'wc-add-to-cart',
+        'woocommerce',
         'wc-cart-fragments',
         'jquery-blockui',
-		'js-cookie'
+        'js-cookie'				
 	);
 
     if ( in_array( $handle, $defer_scripts ) ) {
