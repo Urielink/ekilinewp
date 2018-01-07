14490bfa2f12792ccce22ba85db8c0a1cba3166e
Urielink <urielink81@gmail.com>
2018-01-06 21:54:52 -0600

    añadir el boton de fondeo. corregir la salida de xml para sitemap añadir traduccion de botono de fondeo

--- inc/serviceOptimize.php
+++ inc/serviceOptimize.php
@@ -114,7 +114,41 @@ function ekiline_postcss(){
 }
 add_action( 'wp_head', 'ekiline_postcss', 99);
 
+/** Boton en menu con popwindow
+ * https://codex.wordpress.org/Plugin_API/Action_Reference/wp_before_admin_bar_render
+ * https://codex.wordpress.org/Javascript_Reference/ThickBox
+ * Y con estilos agregados.
+ * https://codex.wordpress.org/Function_Reference/wp_add_inline_style
+ * https://gist.github.com/corvannoorloos/43980115659cb5aee571
+ */
 
+function ekiline_bar_link() {
+	global $wp_admin_bar;
+	if ( !is_super_admin() || !is_admin_bar_showing() )
+		return;	
+	
+		$wp_admin_bar->add_menu( array(
+		'id' => 'goekiline',
+		'title' => __( 'FundMe', 'ekiline'),
+		'href' => 'http://ekiline.com/fondeo/?TB_iframe=true&width=600&height=550',
+		'meta' => array( 
+			'class' => 'gold',
+			'target' => '_blank',
+			'onclick' => 'jQuery(this).addClass("thickbox");'
+			)
+	) );
+} 
+add_action('admin_bar_menu', 'ekiline_bar_link', 100 ); 
+
+function ekiline_admin_styles() {
+	if ( !is_super_admin() )
+		return;	
+	$extracss = '.gold a::before { content: "\f511";} .gold a { background-color: #58aa03 !important; } .gold:hover a { background-color: #ffb900 !important; color: #fff !important; } .gold:hover a::before { content: "\f339"; color: #fff !important; }'; 				    
+    wp_add_inline_style( 'wp-admin', $extracss );
+    wp_add_inline_style( 'ekiline-style', $extracss );
+}
+add_action( 'admin_enqueue_scripts', 'ekiline_admin_styles' );
+add_action( 'wp_enqueue_scripts', 'ekiline_admin_styles' );
 
 /**
  * Optimizar los scripts con async, esta funcion solo requiere el manejador
