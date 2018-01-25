48c1a70a3f28646dbf4319a450ccc03fedb3f330
Urielink <urielink81@gmail.com>
2018-01-24 19:07:47 -0600

    js concatenado!

--- js/ekiline.js
+++ js/ekiline.js
@@ -207,12 +207,12 @@ jQuery(document).ready(function($){
 		$('.popover-rich').each(function(){
 			//extraigo el enlace del contenido
 		    var popHtml = $(this).attr('href');
-		    // Anteriormente el objeto podía tener una disposición oculta
-		    //$( popHtml ).css('display','none'); 
-		    // Ahora es mejor agregar un envoltorio
-		    $( popHtml ).wrap( '<div class="collapse"></div>' );
+			//Creo un envoltorio
+			var popWrap = $('<div/>', { "class" : "collapse" });
+			//Envuelvo
+		    $( popHtml ).wrap( popWrap );
 		});    
-    
+
     
 	/** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 	 * 
@@ -376,20 +376,23 @@ jQuery(document).ready(function($){
 				var dataSrc = $(this).attr('data-src') || null;
 				
 		        // si hay titulo en el boton genero un modal con titulo 
-				var attrTittle = $(this).attr('title') || null;
-		
+				var attrTittle = $(this).attr('title') || null;		
 		
 				// Declaro mis espacios para insertar el tipo de elemento
 		
 		            var contenidoModal = ''; // acorde al tipo de contenido este campo se Agrega. 
 		            
 		            //Abr 28 2017: añado una clase extra al modal para poder maniobrar las medidas por tipo de contenido y le añado un sufijo "linkClass.substr(1)"
-					var modalHtml = '<div class="modal window-'+ linkClass.substr(7) +' fade zoom" id="' + hrefToid + '"><div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-body">' + contenidoModal + '</div></div></div></div>';
+					//var modalHtml = '<div class="modal window-'+ linkClass.substr(7) +' fade zoom" id="' + hrefToid + '"><div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-body">' + contenidoModal + '</div></div></div></div>';
+					var modalHtml = $('<div/>',{'class':'modal window-'+ linkClass.substr(7) +' fade zoom', 'id': hrefToid, 'html': $('<div/>',{ 'class':'modal-dialog modal-lg', 'html': $('<div/>',{ 'class':'modal-dialog modal-lg', 'html': $('<div/>',{ 'class':'modal-content', 'html': $('<div/>',{ 'class':'modal-body', 'html': contenidoModal }) }) }) }) });
 		
-					var modalTitle = '<div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span>&times;</span></button></div>';
+					//var modalTitle = '<div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span>&times;</span></button></div>';
+					var modalTitle = $('<div/>',{'class':'modal-header', 'html': $('<button/>',{'class':'close', 'type': 'button', 'data-dismiss': 'modal', 'html': $('<span/>',{'html':'&times;'}) }) });
+
 		
 					// de momento el footer no se requiere, pero lo dejaremos solo como referencia.
-					var modalFooter = '<div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button></div>';
+					//var modalFooter = '<div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button></div>';
+					var modalFooter = $('<div/>',{'class':'modal-footer', 'html': $('<button/>',{'class':'btn btn-secondary', 'type': 'button', 'data-dismiss': 'modal', 'html':'Cerrar' }) });
 			
 		
 		    	// Precarga la ventana que contendra la informacion
@@ -403,7 +406,8 @@ jQuery(document).ready(function($){
 			        
 			    	// Si hay titulo, Agrega un H4 en el espacio del titulo
 			        if ( attrTittle != null ){
-			            $('#' + hrefToid + ' .modal-content .modal-header').append( '<h4 class="modal-title">' + attrTittle +'</div>' );	        	
+			            //$('#' + hrefToid + ' .modal-content .modal-header').append( '<h4 class="modal-title">' + attrTittle +'</div>' );	        	
+			            $('#' + hrefToid + ' .modal-content .modal-header').append( $('<h4/>', { 'class':'modal-title','html': attrTittle }) );	        	
 			        }
 			        
 			        
@@ -411,7 +415,8 @@ jQuery(document).ready(function($){
 			            	
 					if ( linkClass == '.modal-iframe' ){
 						
-			            contenidoModal = '<iframe frameborder="0" scrolling="yes" allowtransparency="true" src="' + dataSrc + '" width="100%" height="100%"></iframe>';
+			            //contenidoModal = '<iframe frameborder="0" scrolling="yes" allowtransparency="true" src="' + dataSrc + '" width="100%" height="100%"></iframe>';
+			            contenidoModal = $('<iframe/>', { 'frameborder':'0', 'scrolling':'yes', 'allowtransparency': 'true', 'src': dataSrc, 'width':'100%', 'height': '100%' });
 		
 			            $('#' + hrefToid + ' .modal-content .modal-body').html( contenidoModal );
 		
@@ -419,7 +424,8 @@ jQuery(document).ready(function($){
 		
 						dataWidth = '';
 		
-				        contenidoModal = '<img class="img-fluid" src="' + dataSrc + '"/>';
+				        //contenidoModal = '<img class="img-fluid" src="' + dataSrc + '"/>';
+				        contenidoModal = $('<img/>', { 'class':'img-fluid', 'src': dataSrc });
 		
 			            $('#' + hrefToid + ' .modal-content .modal-body').html( contenidoModal );
 		
@@ -529,10 +535,26 @@ jQuery(document).ready(function($){
 	        
 	        var gallery = $( rename ).html();
 	        	                
-	        var modalgallery = '';
-	        modalgallery += '<div class="modal fade zoom" role="dialog" id="galleryModal"><div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-body">';	                            
-	        modalgallery += '<div id="carousel-modal" class="carousel slide carousel-fade" data-ride="carousel" style="display:none;"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button><ol class="carousel-indicators"></ol><div class="carousel-inner" role="listbox">'+ gallery +'</div><a class="carousel-control-prev" href="#carousel-modal" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carousel-modal" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a></a></div>';
-	        modalgallery += '</div></div></div></div>';
+	        //var modalgallery = '';
+	        // modalgallery += '<div class="modal fade zoom" role="dialog" id="galleryModal"><div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-body">';	                            
+	        // modalgallery += '<div id="carousel-modal" class="carousel slide carousel-fade" data-ride="carousel" style="display:none;"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button><ol class="carousel-indicators"></ol><div class="carousel-inner" role="listbox">'+ gallery +'</div><a class="carousel-control-prev" href="#carousel-modal" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carousel-modal" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a></a></div>';
+	        // modalgallery += '</div></div></div></div>';
+	        
+	        //elementos centrales
+			var mgClose = $('<button/>', { 'type':'button', 'class':'close', 'data-dismiss':'modal', 'html': $('<span/>', { 'aria-hidden':'true', 'html': '&times;' }) });				
+			var mgLi = $('<ol/>', { 'class':'carousel-indicators' });
+			var mgGal = $('<div/>', { 'class':'carousel-inner', 'role':'listbox', 'html': gallery });
+	        //controles
+			var pico = $('<span/>', { 'class':'carousel-control-prev-icon', 'aria-hidden':'true' });
+			var pdes = $('<span/>', { 'class':'sr-only', 'aria-hidden':'true', 'html': 'Previous' });								
+			var nico = $('<span/>', { 'class':'carousel-control-next-icon', 'aria-hidden':'true' });
+			var ndes = $('<span/>', { 'class':'sr-only', 'aria-hidden':'true', 'html': 'Next' });
+			var mgPre = $('<a/>', { 'class':'carousel-control-prev', 'href':'#carousel-modal', 'role':'button', 'data-slide':'prev'}).append( pico , pdes );
+			var mgNex = $('<a/>', { 'class':'carousel-control-next', 'href':'#carousel-modal', 'role':'button', 'data-slide':'next'}).append( nico , mgNex );      					
+	        //el conjunto de carrusel
+	        var mgAll = $('<div/>',{ 'id':'carousel-modal', 'class':'carousel slide carousel-fade', 'data-ride':'carousel', 'style':'display:none;'}).append( mgClose , mgLi , mgGal, mgPre, mgNex );
+	        	        
+        	var modalgallery = $('<div/>', { 'class':'modal fade zoom', 'id': 'galleryModal', 'role':'dialog', 'html': $('<div/>', { 'class':'modal-dialog modal-lg','html': $('<div/>', { 'class':'modal-content','html': $('<div/>', { 'class':'modal-body','html': mgAll }) }) }) });
 	        
 	        $( modalgallery ).modal('show');
 	        	        	         
@@ -561,7 +583,8 @@ jQuery(document).ready(function($){
 			            var url = $(this).parent('a').attr('href');
 			            //console.log(url);
 			            // Enero, transformamos todas las imagenes en responsivas
-			            var img = '<img class="img-fluid" src="'+url+'" />';
+			            //var img = '<img class="img-fluid" src="'+url+'" />';
+			            var img = $('<img/>', { 'class':'img-fluid', 'src': url });
 			            // console.log(img);
 			            $(this).replaceWith(img);
 		            });
@@ -597,7 +620,8 @@ jQuery(document).ready(function($){
 	            // creo el loop de contadores            
 	            for (var i=0; i<ns; i++) {
 	                // console.log('intento ' + i);
-	                $(this).find('#galleryModal .carousel-indicators').append('<li data-target="#carousel-modal" data-slide-to="'+i+'"></li>');
+	                //$(this).find('#galleryModal .carousel-indicators').append('<li data-target="#carousel-modal" data-slide-to="'+i+'"></li>');
+	                $(this).find('#galleryModal .carousel-indicators').append( $('<li/>', { 'data-target':'#carousel-modal', 'data-slide-to': i }) );	                
 	            }            
 	            
 	            // al primer slide activalo
