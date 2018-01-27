5d570aad33a7eec630710da6c568cee8f67bf98a
Urielink <urielink81@gmail.com>
2018-01-26 20:06:45 -0600

    Boton para sitemap agregado en admin

--- inc/serviceSitemap.php
+++ inc/serviceSitemap.php
@@ -5,14 +5,64 @@
  * https://blog.jallet.org/generer-un-fichier-sitemap-xml-sans-plugin-490.html
  * https://codex.wordpress.org/Filesystem_API
  * https://www.sitemaps.org/protocol.html
+ * 
+ * Ene añadir un botón que ejecute la tarea.
+ * https://codex.wordpress.org/AJAX_in_Plugins
+ * https://wordpress.stackexchange.com/questions/251584/how-to-make-custom-button-link-on-the-wordpress-admin-bar-run-by-ajax
+ * https://wordpress.stackexchange.com/questions/210701/display-admin-notice-error-message-form-jquery-event
+ * https://wptheming.com/2011/08/admin-notices-in-wordpress/
+ * https://wordpress.stackexchange.com/questions/224485/how-to-pass-parameters-to-admin-notices
+ * https://codex.wordpress.org/Plugin_API/Action_Reference/wp_ajax_(action)
  *
  * @package ekiline
  */
 
-if( true === get_theme_mod('ekiline_sitemap') ) {   
-	add_action( 'publish_post', 'ekiline_sitemap_xml' ) ;
-	add_action( 'publish_page', 'ekiline_sitemap_xml' ) ;
-}	
+if( true === get_theme_mod('ekiline_sitemap') ) {
+ 
+	// crear botón para admin. 
+	add_action('admin_bar_menu', 'add_item', 100);
+	
+	function add_item( $admin_bar ){
+	  global $pagenow;
+	  $admin_bar->add_menu( array( 'id'=>'ekiline-sitemap','title'=>'Sitemap','href'=>'#' ) );
+	}
+	
+	/* Crear la respuesta de actualización con ajax y jQuery */
+	
+	add_action( 'admin_footer', 'ekiline_sitemap_action_js' );
+	
+	function ekiline_sitemap_action_js() { ?>
+	  <script type="text/javascript" >
+	     jQuery("li#wp-admin-bar-ekiline-sitemap .ab-item").on( "click", function() {
+	        var data = { 'action': 'ekiline_sitemap_write' };
+	        /* since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php */
+	        jQuery.post(ajaxurl, data, function(response) {
+	           //alert( response );
+               jQuery('#wpbody-content .wp-header-end').after('<div class="notice notice-success is-dismissible sitemap-notice"><p>'+response+'</p></div>');
+	        });		        	                	
+	      });
+	      
+	  </script> <?php
+	}
+	
+	// function sitemap_notice_success() {
+		// $class = 'notice notice-success is-dismissible';
+		// $message = __( 'Sitemap created!', 'ekiline' );
+		// printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) ); 
+	// }
+	// add_action( 'admin_notices', 'sitemap_notice_success' );
+
+	
+	/* Ejecutar la funcion en cada caso: */
+	// con el adminbar
+	add_action( 'wp_ajax_ekiline_sitemap_write', 'ekiline_sitemap_xml' );
+	
+	// Originalmente: en cada publicación de manera automática (es mejor que el cliente lo ejecute via manual)
+	//add_action( 'publish_post', 'ekiline_sitemap_xml' ) ;
+	//add_action( 'publish_page', 'ekiline_sitemap_xml' ) ;
+
+} 
+
 
 function ekiline_sitemap_xml() {
 
@@ -91,4 +141,12 @@ function ekiline_sitemap_xml() {
 	$file = trailingslashit( $homedir ) . 'sitemap.xml';
 	$wp_filesystem->put_contents( $file, $sitemap, FS_CHMOD_FILE );
 	
+    $response = "Sitemap created!";
+    echo $response;		
+	
+    wp_die(); /* this is required to terminate immediately and return a proper response */
 }
+
+//test
+
+
