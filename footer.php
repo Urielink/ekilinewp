e5c672fb1e5a89edb83d050ab3a52a1efa724f80
Urielink <urielink81@gmail.com>
2018-03-07 16:33:24 -0600

    ajuste en el footer

--- footer.php
+++ footer.php
@@ -24,17 +24,23 @@
 
 </div><!-- #page -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
+	<footer id="colophon" class="site-footer py-2" role="contentinfo">
 	    	    
 	    <!--widgets-->
 	    <div class="container">
 
             <div class="row"><?php dynamic_sidebar( 'footer-w1' ); ?></div>
             
+            
             <div class="site-info text-center clearfix">    
                 <?php printf( esc_html__( '&copy; Copyright %1$s', 'ekiline' ), esc_attr( date('Y') . ' ' . get_bloginfo( 'name', 'display' )) );?> <a class="btn btn-secondary btn-sm float-right" href="#top"><span class="fa fa-chevron-up"></span></a>                    
                 <br/>                
-                <small class="float-left"><?php printf( esc_html__( 'Proudly powered by %s', 'ekiline' ), '<a href="https://wordpress.org/">WordPress</a>' ); ?> <?php printf( esc_html__( 'and %s', 'ekiline' ), '<a href="'.__('http://ekiline.com','ekiline').'" target="_blank">Ekiline</a>' ); ?></small>                
+                <!--small class="float-left"><?php printf( esc_html__( 'Proudly powered by %s', 'ekiline' ), '<a href="https://wordpress.org/">WordPress</a>' ); ?> <?php printf( esc_html__( 'and %s', 'ekiline' ), '<a href="'.__('http://ekiline.com','ekiline').'" target="_blank">Ekiline</a>' ); ?></small-->                
+				<small class="tooltip-default float-left my-2 d-inline-block" tabindex="0" data-toggle="tooltip" data-html="true" title="<small><?php printf( esc_html__( 'Proudly powered by %s', 'ekiline' ), '<a href=\'https://wordpress.org/\'>WordPress</a>' ); ?> <?php printf( esc_html__( 'and %s', 'ekiline' ), '<a href=\''.__('http://ekiline.com','ekiline').'\' >Ekiline</a>' ); ?></small>">
+				  <img src="<?php echo get_template_directory_uri() . '/img/ico-ekiline.png' ?>" style="pointer-events: none;" />
+				</small>
+
+
             </div><!-- .site-info -->   
         
 		</div>
