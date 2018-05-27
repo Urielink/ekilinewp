aa7d3c50932ac6f6f129d2a5a6fb4b410e3784e2
Urielink <urielink81@gmail.com>
2018-05-27 00:06:14 -0500

    ajuste en los estilos del tema hijo

--- functions.php
+++ functions.php
@@ -7,16 +7,14 @@
  * @package ekiline
  */
 
-// Estilos // Styles
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
-
-	// permitir el uso del tema minificado // allow minified file
-	$located = locate_template( 'style.min.css' );
-	if ($located != '' ) {
-		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.min.css', array(), '1.0', 'all' );	
-    } else {
-		wp_enqueue_style( 'parent-style', get_stylesheet_uri() );	
-    }	
-	
-}
+/**
+ * Estilos CSS // Stylesheets CSS
+ * Para usar la hoja de estilos del tema habilitar el siguiente código || For custom CSS enable this code.
+ * Puedes cambiar a tema minificado: style.min.css || You can switch to minified archive: style.min.css
+ */
+// function child_theme_enqueue_styles() {
+    // $parent_style = 'ekiline-style';
+    // wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    // wp_enqueue_style( 'ekiline-child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version') );
+// }
+// add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_styles' );
